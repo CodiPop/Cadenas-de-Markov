01b7ca70930fc1f41bb121f708668be8c3ba2c04
Santiago GM <47578270+CodiPop@users.noreply.github.com>
2020-04-26 21:08:15 -0500

    Fixed txt

--- Markovy.py
+++ Markovy.py
@@ -1,1 +1,17 @@
-print("ffs")
+
+## Cada linea en el documento es una pagina del pdf source de donde se saco el txt.
+
+a = input("K: ")
+b = input("N: ")
+file = open("gabo.txt", "r")
+abc = file.read()
+file.close
+##Shows the entire txt as a String
+
+##Creates a list of the words in the txt
+Lw = abc.split()
+print("********************************************************************************************************************************************")
+##creates a list without duplicates
+Lwnf = list(dict.fromkeys(Lw))
+for i in Lw:
+    print(i, end= " ")
