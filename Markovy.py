697a3821b9107c34b862550db655405fb278c558
Santiago GM <47578270+CodiPop@users.noreply.github.com>
2020-04-28 20:24:22 -0500

    Update Markovy.py
    inciso 1 Finalizado

--- Markovy.py
+++ Markovy.py
@@ -1,43 +1,59 @@
 import numpy as np
+k = int(input("K:"))
+N = int(input("N:"))
 
-abc = open('gabo.txt').read()
+File = open('gabo.txt', encoding='utf8').read()
 
-
-##k = int(input("K:"))
-k=25
-n = int(input("N:"))
-line = abc[:k]
-modelo = abc.split()
-
-def h_pares(modelo):
+modelo = File.split()
+check = False
+line = File[:k]
+ju= len(line)
+print(line[ju-1:])
+if(line[ju-1:] == " "):
+    check=True
+    line = line[0:ju-1]
+    
+def hazpar(modelo):
     for i in range(len(modelo)-1):
         yield (modelo[i], modelo[i+1])
         
-pares = h_pares(modelo)
+pares = hazpar(modelo)
 
-Lkeys = {}
+dictP = {}
 
 for palabra1, palabra2 in pares:
-    if palabra1 in Lkeys.keys():
-        Lkeys[palabra1].append(palabra2)
+    if palabra1 in dictP.keys():
+        dictP[palabra1].append(palabra2)
     else:
-        Lkeys[palabra1] = [palabra2]
-
+        dictP[palabra1] = [palabra2]
+ 
 ppalabra = np.random.choice(modelo)
 
-while ppalabra.islower():
-    ppalabra = np.random.choice(modelo)
+sw=True
+ll=len(line)
+
+while sw:
+    
+    if(line[ll-1] != ppalabra[0]):
+        ppalabra = np.random.choice(modelo)  
+    else:
+        print("son iguales")
+        sw=False
 
 cMarkov = [ppalabra]
 
-for i in range(k):
-    cMarkov.append(np.random.choice(Lkeys[cMarkov[-1]]))
+n_words = N
 
-CADENA = ' '.join(cMarkov)
-casiqueno=line + CADENA
-##Muestra cadna generada
-print(CADENA)
-##Muestra line + la cadena generada con restriccion de longitud N
-print(casiqueno[0:n])
+for i in range(n_words):
+    cMarkov.append(np.random.choice(dictP[cMarkov[-1]]))
 
+uuu=' '.join(cMarkov)
+uuu = uuu[1:len(uuu)]
+
+if check:
+    cunt= line + uuu + " "
+    print(cunt[:N])
+else:
+    cunt= line + uuu 
+    print(cunt[:N])
 
